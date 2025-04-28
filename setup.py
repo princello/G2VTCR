b3f13852bea309ab771dc4fb3c988eb9d25aa74c
Zicheng Wang <zw3672@gmail.com>
2025-04-28 11:25:29 -0400

    Update requirements-dev, and setup

--- setup.py
+++ setup.py
@@ -1,25 +1,29 @@
 from setuptools import setup, find_packages
 
 def parse_requirements(filename):
+    """Parse requirements from a file."""
     with open(filename, "r") as file:
-        return file.read().splitlines()
+        return [line for line in file.read().splitlines() 
+                if not line.startswith('#') and line.strip()]
+
 setup(
     name="g2vtcr",
     version="1.0.0",
     author="Shen Lab at Columbia University",
-    author_email="zw2595l@cumc.columbia.edu",
+    author_email="zw2595@cumc.columbia.edu",
     description="Graph-based Representation and Embedding of Antigen and TCR for Enhanced Recognition Analysis",
     long_description=open("README.md").read(),
     long_description_content_type="text/markdown",
-    url="https://github.com/princello/g2vtcr",    packages=find_packages(),
+    url="https://github.com/princello/g2vtcr",
+    packages=find_packages(),
     install_requires=parse_requirements("requirements.txt"),
     extras_require={
         "dev": parse_requirements("requirements-dev.txt"),
     },
     classifiers=[
         "Programming Language :: Python :: 3",
-        "License :: OSI Approved :: GPL-3.0 License",
+        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
         "Operating System :: OS Independent",
     ],
-    python_requires=">=3.11",
+    python_requires=">=3.8",
 )
